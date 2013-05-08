f2050b715262cc955d53165641f6571e19a8bbf9
xlivsvi <xlivsvi@gmail.com>
2013-05-08 21:33:59 +0900

    Create CastTest.cpp
    多重継承におけるキャストの動作を見るために作成

---
+++ CastTest.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+class InterfaceA
+{
+public:
+  int var_a;
+	virtual void foo0() = 0;
+};
+
+class InterfaceB
+{
+public:
+	int var_b;
+	virtual void foo1() = 0;
+};
+
+class Implement : public InterfaceA,public InterfaceB
+{
+public:
+	virtual void foo0()
+	{
+		puts("call foo0");
+	}
+
+	virtual void foo1()
+	{
+		puts("call foo1");
+	}
+};
+
+int main(int argc, char* argv[] )
+{
+	Implement* pImp = new Implement();
+	InterfaceA* pIa = (InterfaceA*)(pImp);
+	InterfaceB* pIb = (InterfaceB*)(pImp);
+
+	pImp->var_a = 0x55AA;
+	pImp->var_b = 0xCC33;
+	pImp->foo0();
+	pImp->foo1();
+
+	return 0;
+}
+
